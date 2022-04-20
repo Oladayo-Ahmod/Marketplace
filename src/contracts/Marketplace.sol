bf8c07bb5f80b48aed979d2ea8fbaf57c38799e4
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-20 17:49:29 +0100

    create product data

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -3,6 +3,15 @@ pragma solidity >=0.4.21 <0.6.0;
 contract Marketplace {
   string public name;
 
+  // create product data
+  struct Product{
+    uint id;
+    string name;
+    uint price;
+    string owner;
+    bool purchased;
+  }
+
   constructor() public {
     name = "Market Place";
   }
