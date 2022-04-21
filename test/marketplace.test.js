328930054152ff3ab8f33a82691d6a8e2e529be3
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-21 15:14:11 +0100

    test product creation

--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -6,7 +6,8 @@ contract('MarketPlace', (accounts)=>{
     before(async ()=>{
         marketplace = await MarketPlace.deployed()
     })
-
+    
+    // test deployment
     describe('deployment', async () =>{
         it('deployed successfully', async () =>{
             let address = await MarketPlace.address;
@@ -18,6 +19,18 @@ contract('MarketPlace', (accounts)=>{
         it('has a valid name', async () =>{
             const name = await marketplace.name();
             assert.equal(name,'Market Place')
+        })
+        
+    })
+
+    // test product
+    decribe('product', async () =>{
+        let result,productCount
+        before(async () =>{
+            result = await marketplace.create_product()
+        })
+        it('creates product', async () =>{
+            assert.equal(productCount,1)
         })
     })
 })
