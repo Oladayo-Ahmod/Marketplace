b5083e96a2ed2160575fa4825a2355846f51b1a0
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-21 10:19:46 +0100

    create products count

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,7 +2,8 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract Marketplace {
   string public name;
-  
+  // product count
+  uint public products_count = 0;
   // create products mapping : key :value :name
   mapping(uint => Product) public products;
   // create product data
