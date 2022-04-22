e5279cba76db72a03e9cd08e3d49d676b68bcdd4
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-22 12:23:12 +0100

    test for product purchase

--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -47,17 +47,27 @@ contract('MarketPlace', ([deployer,seller,buyer])=>{
         })
 
         // test for failure
-    //    await await marketplace.create_product('',web3.utils.toWei('1','Ether'),{from : seller}).should.be.rejected;
+        //    await await marketplace.create_product('',web3.utils.toWei('1','Ether'),{from : seller}).should.be.rejected;
 
-    // test for listing products
-    it('it lists product', async () =>{
-        const product = await marketplace.products(productCount)
-        assert.equal(product.id.toNumber(),productCount,'id is correct')
-        assert.equal(product.name,'Iphone XR','name is correct')
-        assert.equal(product.owner,seller,'seller is correct')
-    //    assert.equal(product.price,'10','id is correct')
-           assert.equal(product.purchased,false,'purchased is correct')  
-    })
-
+        // test for listing products
+        it('it lists product', async () =>{
+            const product = await marketplace.products(productCount)
+            assert.equal(product.id.toNumber(),productCount,'id is correct')
+            assert.equal(product.name,'Iphone XR','name is correct')
+            assert.equal(product.owner,seller,'seller is correct')
+        //    assert.equal(product.price,'10','id is correct')
+            assert.equal(product.purchased,false,'purchased is correct')  
+        })
+        
+        // test for product purchase
+        it('it purchases product', async () =>{
+            assert.equal(productCount,1)
+           const event = result.logs[0].args
+           assert.equal(event.id.toNumber(),productCount,'id is correct')
+           assert.equal(event.name,'Iphone XR','name is correct')
+           assert.equal(event.owner,buyer,'seller is correct')
+        //    assert.equal(event.price,'10','id is correct')
+           assert.equal(event.purchased,true,'purchased is correct')
+        })
     })
 })
