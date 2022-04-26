24e6246a9fcd71bbf57c4aa5cfbe190413bba9e2
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-26 23:02:12 +0100

    loading web3 for fronteend

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,10 @@ import './App.css';
 
 class App extends Component {
 
+  async componentWillMount(){
+    await this.loadweb3()
+  }
+
   async loadweb3(){
     if(window.ethereum){
       window.web3 = new Web3(window.ethereum)
