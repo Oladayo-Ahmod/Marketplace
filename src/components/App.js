ec3d17049598598b1f00001d8eb342231ea34400
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-26 23:01:08 +0100

    loading web3 for fronteend

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+
+  async loadweb3(){
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('non supported browser')
+    }
+  }
+
   render() {
     return (
       <div>
