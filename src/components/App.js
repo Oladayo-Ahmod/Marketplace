701b987d3b94707411730f10a79e315558992f77
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-28 16:00:52 +0100

    create navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -52,17 +52,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            MarketPlace 
-          </a>
-          <p className="text-white text-sm mt-3">{this.state.account} </p>
-        </nav>
+       
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,19 @@ import React, {component} from 'react'
 class Navbar extends Component{
     
     render(){
-        return{
-            
-        }
+        return (
+            <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+            <a
+              className="navbar-brand col-sm-3 col-md-2 mr-0"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              MarketPlace 
+            </a>
+            <p className="text-white text-sm mt-3">{this.state.account} </p>
+          </nav>
+        );
     }
 }
 
