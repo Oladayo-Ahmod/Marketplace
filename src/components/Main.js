2d77ba2ffd9d9370b494f1aa680384f923fb843f
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-04-30 21:15:17 +0100

    product set u

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,17 @@ class Main extends Component{
     
     render(){
         return (
-            <div className="product text-secondary">
-                <h2>Add product</h2>
+            <div className="col-md-5">
+                <form class="form-group" novalidate>
+                    <h3 className="text-secondary">Add Product</h3>
+                    <label for="validationCustom01" class="form-label">Product name</label>
+                    <input type="text" class="form-control" id="validationCustom01" value="Mark" required />
+                    <label for="validationCustom01" class="form-label">Product price</label>
+                    <input type="number" class="form-control" id="validationCustom01" value="Mark" required />
+                  <div class="col-12">
+                    <button class="btn btn-primary" type="submit">Submit form</button>
+                  </div>
+                </form>
             </div>
         );
     }
